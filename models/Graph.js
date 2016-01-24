66085e4d387178452c4b3dc11f07e07af0f5a3aa
Jacky <jaackyvan@gmail.com>
2016-01-24 13:37:26 -0500

    Updating Graph model type to be an array

--- models/Graph.js
+++ models/Graph.js
@@ -5,7 +5,7 @@ var graphSchema = new Schema({
 	owner: { type: Schema.Types.ObjectId, ref: 'User' },
 	dateCreated : { type: Date, required: true, default: Date.now },
 	name : { type: String, required: true, default: Date.now },
-	data : { type: Schema.Types.Mixed, required: true, default: [] }
+	data : [{ type: Schema.Types.Mixed, required: true, default: [] }]
 });
 
 module.exports = mongoose.model('Graph', graphSchema);
