d86c703097e34f100e3085d8f15b4eebaabfc778
Jacky <jaackyvan@gmail.com>
2016-01-24 12:40:22 -0500

    Updating Graph model

--- models/Graph.js
+++ models/Graph.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var graphSchema = new Schema({
-	owner: { Schema.ObjectId, ref: 'User' }
+	owner: { type: Schema.Types.ObjectId, ref: 'User' },
 	dateCreated : { type: Date, required: true, default: Date.now },
-	data : [ Number ]
+	name : { type: String, required: true, default: Date.now },
+	data : { type: Schema.Types.Mixed, required: true, default: [] }
 });
 
 module.exports = mongoose.model('Graph', graphSchema);
