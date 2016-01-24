25ecc8101da62c18e2d23d96e3b3e2c12fb6bae1
Jacky <jaackyvan@gmail.com>
2016-01-24 14:44:10 -0500

    Adding functionality to add new points to a graph

--- routes/graph.js
+++ routes/graph.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var flash = require('connect-flash');
 var User = require('../models/User.js');
 var Graph = require('../models/Graph.js');
+var moment = require('moment');
 
 /* Check if user is logged in */
 router.use(function(req, res, next) {
@@ -15,7 +16,29 @@ router.use(function(req, res, next) {
 
 router.get('/:id', function(req, res) {
 	Graph.findOne({ _id: req.params.id }, function(err, graph) {
-		res.render('graph', { graph: JSON.stringify(graph), graphName: graph.name });
+		res.render('graph', { graph: JSON.stringify(graph), graphName: graph.name, graphId: req.params.id });
+	});
+});
+
+router.post('/input', function(req, res) {
+	Graph.findOne({ _id: req.body.id }, function(err, graph) {
+		console.log("error: " + err);
+		console.log(graph);
+		var dataPoint = [
+			parseInt(moment(req.body.date, 'ddd MMM D, YYYY').format('x')),
+			parseInt(req.body.value)
+		];
+		console.log(dataPoint);
+		graph.data.push(dataPoint);
+		console.log(graph.data);
+		graph.save(function(err, graph) {
+			if (err) {
+				console.log("save error: " + err);
+			} else {
+				console.log("after save graph: ", graph);
+				res.redirect('/graph/' + req.body.id);
+			}
+		});
 	});
 });
 
