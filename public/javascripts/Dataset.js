3e5ff596aa8478ac9c0a4fb61701ff35050480c0
Jacky <jaackyvan@gmail.com>
2016-01-24 16:08:33 -0500

    Adding starter skeleton for Dataset prototype object

---
+++ public/javascripts/Dataset.js
@@ -0,0 +1,45 @@
+function Dataset(data) {
+	this.data = data;
+}
+
+/*
+* Return dataset of the current week
+*/
+Dataset.prototype.getCurretWeek() {
+
+}
+
+/*
+* Return dataset of the current month
+*/
+Dataset.prototype.getCurrentMonth() {
+
+}
+
+/*
+* Return dataset of the current year
+*/
+Dataset.prototype.getCurrentYear() {
+
+}
+
+/*
+* Return the dataset from num weeks from current date
+*/
+Dataset.prototype.backPreviousWeeks(num) {
+
+}
+
+/*
+* Return the dataset from num months from current date
+*/
+Dataset.prototype.backPreviousMonths(num) {
+
+}
+
+/*
+* Return the dataset from num years from current date
+*/
+Dataset.prototype.backPreviousYears(num) {
+
+}
