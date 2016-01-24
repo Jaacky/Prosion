807856bc496c94aa36a3468b1a0895bae4f5e3d5
Jacky <jaackyvan@gmail.com>
2016-01-24 17:17:09 -0500

    Fixed prototype function declarations

--- public/javascripts/Dataset.js
+++ public/javascripts/Dataset.js
@@ -19,7 +19,7 @@ function loop(condition) {
 /*
 * Return dataset of the current year
 */
-Dataset.prototype.getCurrentYear() {
+Dataset.prototype.getCurrentYear = function() {
 	var currentYear = moment().year();
 	var currentYearCondition = function(d) {
 		if (currentYear == d) {
@@ -35,34 +35,34 @@ Dataset.prototype.getCurrentYear() {
 /*
 * Return dataset of the current month
 */
-Dataset.prototype.getCurrentMonth() {
+Dataset.prototype.getCurrentMonth = function() {
 
 }
 
 /*
 * Return dataset of the current week
 */
-Dataset.prototype.getCurretWeek() {
+Dataset.prototype.getCurretWeek = function() {
 
 }
 
 /*
 * Return the dataset from num years from current date
 */
-Dataset.prototype.backPreviousYears(num) {
+Dataset.prototype.backPreviousYears = function(num) {
 
 }
 
 /*
 * Return the dataset from num months from current date
 */
-Dataset.prototype.backPreviousMonths(num) {
+Dataset.prototype.backPreviousMonths = function(num) {
 
 }
 
 /*
 * Return the dataset from num weeks from current date
 */
-Dataset.prototype.backPreviousWeeks(num) {
+Dataset.prototype.backPreviousWeeks = function(num) {
 
 }
