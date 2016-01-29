5327e0654d6246e53bb7db3c028b455833bf939d
Jacky <jaackyvan@gmail.com>
2016-01-28 21:37:36 -0500

    Adding basic fusion model

---
+++ models/Fusion.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var fusionSchema = new Schema({
+	owners : { type: [Schema.Types.ObjectId, ref: 'User'], required: true },
+	dateCreated : { type: Date, required: true, default: Date.now },
+	name : { type: String, required: true, default: Date.now },
+	graphs : { type: Schema.Types.Mixed, required: true }
+});
+
+module.exports = mongoose.model('Fusion', fusionSchema);
