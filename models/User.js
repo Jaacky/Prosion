5c824c8fb170b6e23dc9dd8909a853760ee8614a
Jacky <jaackyvan@gmail.com>
2016-01-30 00:43:40 -0500

    Updating User model to include fusions

--- models/User.js
+++ models/User.js
@@ -12,7 +12,8 @@ var userSchema = new Schema({
 	image : { type: String, default: 'images/avatar.png' },
 	superAdmin: { type: Boolean, default: false },
 	admin: { type: Boolean, default: false },
-	graphs : [ { type: Schema.Types.ObjectId, ref: 'Graph' } ]
+	graphs : [ { type: Schema.Types.ObjectId, ref: 'Graph' } ],
+	fusions : [ { type: Schema.Types.ObjectId, ref: 'Fusion' } ]
 });
 
 userSchema.plugin(passportLocalMongoose);
