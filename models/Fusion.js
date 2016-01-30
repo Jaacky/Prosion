3641573b7a9f624b8d375a484843123127c2bc99
Jacky <jaackyvan@gmail.com>
2016-01-30 00:42:22 -0500

    Adding Fusion model

--- models/Fusion.js
+++ models/Fusion.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var fusionSchema = new Schema({
-	owners : { type: [Schema.Types.ObjectId, ref: 'User'], required: true },
+	owners : [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
 	dateCreated : { type: Date, required: true, default: Date.now },
 	name : { type: String, required: true, default: Date.now },
-	graphs : { type: Schema.Types.Mixed, required: true }
+	graphs : { type: Schema.Types.Mixed, required: true } // [ { id: userID, graphs: [graphID, ...] }, ... ]
 });
 
 module.exports = mongoose.model('Fusion', fusionSchema);
