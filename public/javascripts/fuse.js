3762f8d63906429dc6a2ead00424d77db8183783
Jacky <jaackyvan@gmail.com>
2016-02-06 14:38:11 -0500

    In fuse view, creating new input DOM elements rather than creating them out of strings

--- public/javascripts/fuse.js
+++ public/javascripts/fuse.js
@@ -1,13 +1,25 @@
+function populateDropdownOwn(input) {
+	console.log(input.value);
+}
 
 $('.btn-add-graph').on('click', function(e) {
 	e.preventDefault();
 	var num = $(this).parents('#fuseForm').find('.graph-input').length + 1;
 	$('#numGraphs').val(num);
-	var graph_input = '<input class="form-control graph-input" name="graph-id' + num + '" placeholder="Graph ' + num + '">';
+
+	// Creating input element
+	var graph_input = document.createElement('input');
+	graph_input.className += "form-control graph-input";
+	graph_input.setAttribute('name', 'graph-id' + num);
+	graph_input.setAttribute('placeholder', 'Graph ' + num);
+	graph_input.setAttribute('list', 'myGraphs');
+	graph_input.setAttribute('onkeyup', 'populateDropdownOwn(this)');
+
 	var parent = $(this).parent();
 	if (parent.attr('id') == 'own-graphs') {
 		parent.children('.graph-input:last').after(graph_input);
 	} else {
 		parent.children('.graph-input:last').after(graph_input);
 	}
+
 });
