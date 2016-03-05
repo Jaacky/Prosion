202dc24e41bd78b05287387dbcaf25b8695a728c
Jacky <jaackyvan@gmail.com>
2016-03-05 01:42:21 -0500

    Adding new layout.js for layout view functions

---
+++ public/javascripts/layout.js
@@ -0,0 +1,26 @@
+function populateSearchFriendsOptions(input, selection, url) {
+	var select = $(selection);
+	select.empty();
+	console.log(url + input);
+	$.post(url + input , function(data) {
+		console.log(data);
+		for (var i=0; i<data.length; i++) {
+			var option = document.createElement('option');
+			option.innerHTML = data[i].name;
+			// option.value = data[i].name;
+			option.value = data[i]._id;
+			option.setAttribute('id', data[i]._id);
+			select.append(option);
+		}
+		select.chosen();
+		// $('#searchFriendsContainer .chosen-search input').on('keyup', function() {
+		// 	console.log($("#searchFriendsName").chosen().val());
+		// });
+		select.on('change', function(e, params) {
+			console.log(e);
+			console.log(params);
+		});
+		// select.trigger('chosen:updated');
+		// select.trigger('chosen:open');
+	});
+}
